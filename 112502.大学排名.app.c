b49e56fdb07927467b6e43fb30695358c411924b
sorrymaker77 <74294656+sorrymaker77@users.noreply.github.com>
2020-11-25 16:36:03 +0800

    Create 112502.大学排名.app.c

---
+++ 112502.大学排名.app.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void main(){
+printf("全球大学排名\n");
+int a;
+scanf("%d",&a);
+if(a==1){
+printf("麻省理工大学\n");
+}
+else if(a==2){
+printf("哈佛大学\n");
+}
+else if(a==3){
+printf("剑桥大学\n");
+}
+else if(a==4){
+printf("斯坦福大学\n");
+}
+else if(a==5){
+printf("加州理工大学\n");
+}
+else if(a==6){
+printf("牛津大学\n");
+}
+else if(a==7){
+printf("伦敦大学学院\n");
+}
+else if(a==8){
+printf("帝国理工\n");
+}
+else if(a==9){
+printf("苏黎世联邦理工学院\n");
+}
+else if(a==10){
+printf("广西科技师范学院\n");
+}
+else{
+printf("输入错误 请重新输入\n");
+}
+
+}
